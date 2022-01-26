de784563685cb54ed8de462885fe72af5d52d605
Tixonyia <220220220220@rambler.ru>
2022-01-26 18:24:12 +0300

    Db

--- practice/common/variables.py
+++ practice/common/variables.py
@@ -1,21 +1,21 @@
 import logging
 
-# Порт поумолчанию для сетевого ваимодействия
+# Порт по умолчанию для сетевого взаимодействия
 DEFAULT_PORT = 7777
 # IP адрес по умолчанию для подключения клиента
 DEFAULT_IP_ADDRESS = '127.0.0.1'
 # Максимальная очередь подключений
 MAX_CONNECTIONS = 5
-# Максимальная длинна сообщения в байтах
+# Максимальная длина сообщения в байтах
 MAX_PACKAGE_LENGTH = 1024
 # Кодировка проекта
 ENCODING = 'utf-8'
 # Текущий уровень логирования
 LOGGING_LEVEL = logging.DEBUG
 # База данных для хранения данных сервера:
-SERVER_CONFIG = 'server.ini'
+SERVER_DATABASE = 'sqlite:///server_base.db3'
 
-# Прококол JIM основные ключи:
+# Протокол JIM основные ключи:
 ACTION = 'action'
 TIME = 'time'
 USER = 'user'
@@ -30,19 +30,10 @@ ERROR = 'error'
 MESSAGE = 'message'
 MESSAGE_TEXT = 'mess_text'
 EXIT = 'exit'
-GET_CONTACTS = 'get_contacts'
-LIST_INFO = 'data_list'
-REMOVE_CONTACT = 'remove'
-ADD_CONTACT = 'add'
-USERS_REQUEST = 'get_users'
 
 # Словари - ответы:
 # 200
 RESPONSE_200 = {RESPONSE: 200}
-# 202
-RESPONSE_202 = {RESPONSE: 202,
-                LIST_INFO:None
-                }
 # 400
 RESPONSE_400 = {
             RESPONSE: 400,
